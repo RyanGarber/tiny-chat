1a3e211cd6b39e766d66af3ae90d5e4a9fc31333
Ryan <ryanmichaelgarber@gmail.com>
2026-02-24 03:03:24 -0500

    fix: conditional compilation breaking ci build

--- apps/tauri/src/lib.rs
+++ apps/tauri/src/lib.rs
@@ -1,15 +1,14 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let builder = tauri::Builder::default()
+    let mut _builder = tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_opener::init());
 
     #[cfg(any(target_os = "macos", target_os = "linux", target_os = "windows"))]
     {
-        let builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
+        _builder = _builder.plugin(tauri_plugin_updater::Builder::new().build());
     }
 
-    builder.run(tauri::generate_context!())
-        .unwrap();
+    _builder.run(tauri::generate_context!()).unwrap();
 }

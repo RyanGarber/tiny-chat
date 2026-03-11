d7d8a5d874f58016cd059b1de11f824a04987d08
Ryan <ryanmichaelgarber@gmail.com>
2026-03-11 06:54:48 -0400

    fix: pnpm compatibility with apps/web

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,6 +1,5 @@
 import Fastify from 'fastify';
 import FastifyStatic from '@fastify/static';
-import { fileURLToPath } from 'url';
 import { resolve } from 'path';
 import { config } from 'dotenv';
 
@@ -9,10 +8,10 @@ config({ path: '../../.env' });
 const fastify = Fastify();
 
 fastify.register(FastifyStatic, {
-  root: resolve(fileURLToPath(import.meta.url), '../dist'),
+  root: resolve(import.meta.dirname, '../dist'),
 });
 
-fastify.listen({ port: parseInt(process.env.VITE_WEB_PORT) }, (err, address) => {
+fastify.listen({ port: parseInt(process.env.VITE_WEB_PORT!) }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
